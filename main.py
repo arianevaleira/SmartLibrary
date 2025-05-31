be0d2b1ba27f22462dc079f1b2cefe16968d83d2
arianevaleira <costa.valeria@escolar.ifrn.edu.br>
2025-05-31 16:21:11 -0300

    Corrigindo Erros

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI, HTTPException
 from typing import List
 import uuid
 from datetime import date
 from models import Usuario, Livro, Armario, Sala, Emprestimo
 
+app = FastAPI() 
 
 #Criar os endpointes 
 livro:list[Livro] = [] 
