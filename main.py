27423dd2fc11dde9b5e8ba544604c81a60a85ace
Ariane valeria <153088012+arianevaleira@users.noreply.github.com>
2025-06-06 06:06:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from models import Usuario, Livro, Armario, Sala, Emprestimo, TipoUsuario, Statu
 
 app = FastAPI()
 
-#Banco de Briincadeira
+#Banco de Brincadeira
 livros_db: List[Livro] = []
 usuarios_db: List[Usuario] = []
 armarios_db: List[Armario] = []
